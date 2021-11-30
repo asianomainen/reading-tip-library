026cb584baa0e810084f7f5953f2ac58f08a60ce
Juho Lukka <juholukka00@gmail.com>
2021-11-30 12:13:04 +0200

    switch tip_repository to use sqlite

--- src/repositories/tip_repository.py
+++ src/repositories/tip_repository.py
@@ -1,16 +1,26 @@
+from entities.tip import Tip
+from database_connection import get_database_connection
 
 
 class TipRepository:
     def __init__(self):
-        self.tips =[]
+        self._connection = get_database_connection()
 
     def create_tip(self, tip):
-        self.tips.append(tip)
+        sql = "INSERT INTO Tips (name, url) VALUES (?, ?)"
+        self._connection.execute(sql, (tip.name, tip.url))
+        self._connection.commit()
+
 
     def find_all(self):
-        return self.tips
+        tips = []
+        sql = "SELECT * FROM Tips"
+        result = self._connection.execute(sql)
+        for row in result:
+            tips.append(Tip(row["name"],row["url"]))
+        return tips
 
     def clear(self):
-        self.tips = []
+        sql = "DELETE FROM Tips"
 
 tip_repository = TipRepository()
